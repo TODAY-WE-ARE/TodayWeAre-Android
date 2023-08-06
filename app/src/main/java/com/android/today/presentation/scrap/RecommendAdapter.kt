d20ce75c9f7bd16f6005983458aed89f64be1518
Kim hajeong <happykhj67@gmail.com>
2023-08-06 21:25:24 +0900

    [feat] recommend 어댑터추가

---
+++ app/src/main/java/com/android/today/presentation/scrap/RecommendAdapter.kt
@@ -0,0 +1,46 @@
+package com.android.today.presentation.scrap
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.android.today.data.Recommend
+import com.android.today.databinding.ItemRecommendBinding
+import com.android.today.util.extension.ItemDiffCallback
+
+class RecommendAdapter :
+    ListAdapter<Recommend, RecommendAdapter.RecommendationViewHolder>(recommendationDiffUtil) {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): RecommendationViewHolder {
+        val itemRecommendRecommendedBinding =
+            ItemRecommendBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        return RecommendationViewHolder(
+            itemRecommendRecommendedBinding
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecommendationViewHolder, position: Int) {
+        holder.onBind(getItem(position))
+    }
+
+    class RecommendationViewHolder(private val binding: ItemRecommendBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun onBind(data: Recommend) {
+            binding.data = data
+        }
+    }
+
+    companion object {
+        private val recommendationDiffUtil = ItemDiffCallback<Recommend>(
+            onItemsTheSame = { old, new -> old.image == new.image },
+            onContentsTheSame = { old, new -> old == new }
+        )
+    }
+}
