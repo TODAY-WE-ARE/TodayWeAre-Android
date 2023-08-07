54cd0e5a16d5dd5eba3bee8e4008a5004638220e
Kim-Yun-Seo <rlagrace410@gmail.com>
2023-08-07 23:41:40 +0900

    add branch feature/diary-ui feat: diary_list, diary fragment, xml

--- app/src/main/java/com/android/today/presentation/diary/DiaryFragment.kt
+++ app/src/main/java/com/android/today/presentation/diary/DiaryFragment.kt
@@ -1,8 +1,26 @@
 package com.android.today.presentation.diary
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.android.today.R
+import com.android.today.databinding.FragmentDiaryBinding
+import com.android.today.util.binding.BindingFragment
 
-class DiaryFragment : Fragment()
+class DiaryFragment :
+    BindingFragment<FragmentDiaryBinding>(R.layout.fragment_diary_list) {
+    private val diaryViewModel: DiaryViewModel by viewModels()
 
-//second commit test
-//diary branch 생성 commit 김윤서
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.vm = diaryViewModel
+    }
+
+    private fun initDiaryBtnClickListener() {
+        binding.tvDiary.setSingleOnClickListener {
+            startActivity(Intent(requireActivity(), DiaryActivity::class.java))
+        }
+    }
+}
+
