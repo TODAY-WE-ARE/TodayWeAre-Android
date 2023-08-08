4b9948380b830d2fbd322156e5dd6e6f8bf1aa4a
yunwoooo <20221338@sungshin.ac.kr>
2023-08-08 13:41:06 +0900

    [feat] 추천, 찜 ui

--- app/src/main/java/com/android/today/presentation/scrap/ScrapFragment.kt
+++ app/src/main/java/com/android/today/presentation/scrap/ScrapFragment.kt
@@ -1,5 +1,25 @@
 package com.android.today.presentation.scrap
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.android.today.R
+import com.android.today.databinding.FragmentScrapBinding
+import com.android.today.util.binding.BindingFragment
 
-class ScrapFragment : Fragment()
+class ScrapFragment :
+    BindingFragment<FragmentScrapBinding>(R.layout.fragment_scrap) {
+    private val scrapViewModel: ScrapViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.vm=scrapViewModel
+     }
+
+    private fun initScrapBtnClickListener() {
+        binding.tvMainBoxChu.setSingleOnClickListener {
+            startActivity(Intent(requireActivity(), RecommendFragment::class.java))
+        }
+    }
+}
