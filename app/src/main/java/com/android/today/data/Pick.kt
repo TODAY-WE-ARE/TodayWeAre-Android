49fae5d4d67b0599a315257c91afc14501c5bf49
yunwoooo <20221338@sungshin.ac.kr>
2023-08-13 15:08:55 +0900

    [add] pick 데이터클래스 추가

---
+++ app/src/main/java/com/android/today/data/Pick.kt
@@ -0,0 +1,10 @@
+package com.android.today.data
+
+data class Pick(
+    val image: String,
+    val place: String,
+    val km: String,
+    val content1: String,
+    val content2: String,
+    val content3: String
+)
