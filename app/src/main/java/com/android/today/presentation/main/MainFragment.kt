4873abe37fefce23e78b23e0b44a9b32ec9856b0
Kim hajeong <happykhj67@gmail.com>
2023-08-13 15:03:55 +0900

    [fix] MainFragment 수정

---
+++ app/src/main/java/com/android/today/presentation/main/MainFragment.kt
@@ -0,0 +1,24 @@
+package com.android.today.presentation.main
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import com.android.today.R
+import com.android.today.databinding.FragmentMainBinding
+import com.android.today.presentation.result.ResultFragment
+import com.android.today.util.binding.BindingFragment
+import com.android.today.util.extension.setSingleOnClickListener
+
+class MainFragment : BindingFragment<FragmentMainBinding>(R.layout.fragment_main) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initResultBtnClickListener()
+    }
+
+    private fun initResultBtnClickListener() {
+        binding.tvMainRecommend.setSingleOnClickListener {
+            startActivity(Intent(requireActivity(), ResultFragment::class.java))
+        }
+    }
+}
