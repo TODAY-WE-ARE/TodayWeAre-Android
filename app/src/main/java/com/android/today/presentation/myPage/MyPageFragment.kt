75c59ffab9ccffb64f3a72907013d8519ecb72c9
Kim hajeong <happykhj67@gmail.com>
2023-08-17 16:43:44 +0900

    [fix] 마이페이지 프래그먼트 수정

--- app/src/main/java/com/android/today/presentation/myPage/MyPageFragment.kt
+++ app/src/main/java/com/android/today/presentation/myPage/MyPageFragment.kt
@@ -1,5 +1,14 @@
 package com.android.today.presentation.myPage
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.View
+import com.android.today.R
+import com.android.today.databinding.FragmentMyPageBinding
+import com.android.today.util.binding.BindingFragment
 
-class MyPageFragment : Fragment()
+class MyPageFragment : BindingFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
+    }
+}
